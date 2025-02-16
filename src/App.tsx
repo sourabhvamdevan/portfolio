847e75cebdc8e2d25702ea46a6f5b495cd947b57
Sourabh Vamdevan <44829214+sourabhvamdevan@users.noreply.github.com>
2025-02-16 11:12:17 +0530

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -39,7 +39,7 @@ function App() {
         
         <footer className="bg-gray-900 text-white py-8">
           <div className="container mx-auto px-4 text-center">
-            <p>© 2024 Sourabh Vamdevan. All rights reserved.</p>
+            <p>© 2025 Sourabh Vamdevan. All rights reserved.</p>
           </div>
         </footer>
       </main>
